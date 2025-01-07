c645a7ea39ee5ed37129e0abc3d1895efe903b20
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2025-01-07 00:57:46 -0500

    correccion de errores para eliminar plato

--- src/main/java/com/resturante/logica/models/Plato.java
+++ src/main/java/com/resturante/logica/models/Plato.java
@@ -1,5 +1,6 @@
 package com.resturante.logica.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -8,8 +9,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,5 +33,8 @@ public class Plato {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "menu_id")
     private Menu menu;
+
+    @OneToMany(mappedBy = "plato", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DetallePedido> detalles = new ArrayList<>();
 
 }
